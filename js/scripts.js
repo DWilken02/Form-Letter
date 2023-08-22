c88a2ace4a5cfe6b28332b0f67bb1d84192b3948
DWilken02 <Dylan.Wilken06@gmail.com>
2023-08-22 15:59:03 -0700

    update JavaScript and html files and add bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,11 @@ window.onload = function() {
   form.onsubmit = function(event) {
     event.preventDefault();
     const user1Response = document.getElementById("user1Input").value;
+    const user2Response = document.getElementById("user2Input").value;
 
     document.querySelector("span#user-1-response").innerText = user1Response;
-
+    document.querySelector("span#user-2-response").innerText = user2Response;
+    console.log(user2Response);
     document.querySelector("div#story").removeAttribute("class");
   }
 }
