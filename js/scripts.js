db39969f8a0d31ce6c24950722ca2fc392e67cc3
DWilken02 <Dylan.Wilken06@gmail.com>
2023-08-22 12:54:32 -0700

    make updates to JavaScript and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
-    document.querySelector("span#user1").innerText = "user 1";
+    event.preventDefault();
+    const user1Response = document.getElementById("user1Input").value;
+
+    document.querySelector("span#user-1-response").innerText = user1Response;
 
     document.querySelector("div#story").removeAttribute("class");
   }
